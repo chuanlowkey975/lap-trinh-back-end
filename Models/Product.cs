e306ba671899030e01aa6ca93d98f572ba9b6a1b
chuanvdn975 <160866751+chuanlowkey975@users.noreply.github.com>
2025-04-23 23:13:25 +0700

    Delete Models directory

--- Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LAB03.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
